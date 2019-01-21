4e68e8e7f272b48b6c9a3eb7db00fdc82917c926
crashsol <47147551@qq.com>
2019-01-21 10:04:54 +0800

    添加Open Close Priciple（开放关闭原则）

---
+++ OCP/DeveloperReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCP
+{
+  public  class DeveloperReport
+    {
+        public int Id  { get; set; }
+
+        public string Name { get; set; }
+
+        public string Level { get; set; }
+        public int WorkingHours { get; set; }
+
+        public double HourlyRate { get; set; }
+    }
+}
---
+++ OCP/SalaryCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCP
+{
+   public class SalaryCalculator
+    {
+        private readonly IEnumerable<DeveloperReport> _developerReports;
+
+        public SalaryCalculator(List<DeveloperReport> developerReports)
+        {
+            _developerReports = developerReports;
+        }
+
+        public double CalculateTotalSalaries()
+        {
+            double totalSalaries = 0D;
+            foreach (var item in _developerReports)
+            {
+                totalSalaries += item.HourlyRate * item.WorkingHours;
+            }
+            return totalSalaries;
+        }
+    }
+}
